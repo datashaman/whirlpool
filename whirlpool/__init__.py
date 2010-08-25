beb012316c19af50359774eed066e01f91815c4f
Marlin Forbes <marlinf@datashaman.com>
2010-08-25 20:16:02 +0200

    Rename main class

--- whirlpool/__init__.py
+++ whirlpool/__init__.py
@@ -33,3 +33,31 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 __author__ = 'Marlin Forbes <marlinf@datashaman.com>'
+
+import time, redis
+
+FETCH_INTERVAL = 0.0001
+
+class Whirlpool(redis.Redis):
+    def __init__(self, opts={}):
+        self.opts = dict(host='localhost', port=6379, db=0)
+        self.opts.update(opts)
+        super(Whirlpool, self).__init__(**self.opts)
+	 
+    def take(self, template):
+        while True:
+            matches = self.keys(template)
+
+            if len(matches) > 0:
+                results = {}
+                for match in matches:
+                    results[match] = self.get(match)
+                    self.delete(match)
+                return results
+
+            time.sleep(FETCH_INTERVAL)
+         
+    def read(self, template):
+        while self.exists(template) == 0:
+            time.sleep(FETCH_INTERVAL)
+        return self.get(template)
